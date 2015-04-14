a871c60f9bc16bbd2d81d9603f443648cf003bf9
Michael Feldstein <msfeldstein@gmail.com>
2015-04-13 21:00:40 -0700

    Use deprecated Camera api to run on old devices

--- app/src/main/java/co/extrastrength/shadycam/GLSLCameraView.java
+++ app/src/main/java/co/extrastrength/shadycam/GLSLCameraView.java
@@ -3,8 +3,7 @@ package co.extrastrength.shadycam;
 import android.content.Context;
 import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
-import android.hardware.camera2.*;
-import android.hardware.camera2.params.*;
+import android.hardware.Camera;
 import android.opengl.GLSurfaceView;
 import android.os.Handler;
 import android.util.AttributeSet;
@@ -14,6 +13,7 @@ import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.View;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 
@@ -26,10 +26,7 @@ public class GLSLCameraView extends GLSurfaceView implements GLSLRenderer.Surfac
     private String mCameraId;
     private SurfaceTexture mCameraSurfaceTexture;
     private Size mPreviewSize;
-    private CaptureRequest.Builder mPreviewRequestBuilder;
-    private CaptureRequest mPreviewRequest;
-    private CameraCaptureSession mCaptureSession;
-    private CameraDevice mCameraDevice;
+
     private Handler mCaptureHandler = new Handler();
     private Handler mBackgroundHandler = new Handler();
 
@@ -40,8 +37,6 @@ public class GLSLCameraView extends GLSurfaceView implements GLSLRenderer.Surfac
         mRenderer = new GLSLRenderer();
         mRenderer.setSurfaceListener(this);
         setRenderer(mRenderer);
-        setUpCameraOutputs();
-        openCamera();
         setOnTouchListener(new OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -54,7 +49,7 @@ public class GLSLCameraView extends GLSurfaceView implements GLSLRenderer.Surfac
     }
 
     void tryCreatePreview() {
-        if (mCameraDevice != null && mCameraSurfaceTexture != null)
+        if (mCameraSurfaceTexture != null)
             createCameraPreviewSession();
     }
     @Override
@@ -63,99 +58,15 @@ public class GLSLCameraView extends GLSurfaceView implements GLSLRenderer.Surfac
         tryCreatePreview();
     }
 
-    private CameraDevice.StateCallback mCameraCallback = new CameraDevice.StateCallback() {
-        @Override
-        public void onOpened(CameraDevice camera) {
-            mCameraDevice = camera;
-            tryCreatePreview();
-        }
-
-        @Override
-        public void onDisconnected(CameraDevice camera) {
-            camera.close();
-            mCameraDevice = null;
-        }
-
-        @Override
-        public void onError(CameraDevice camera, int error) {
-            Log.e(TAG, "ERROR " + camera + error);
-        }
-    };
-
-    private void setUpCameraOutputs() {
-        CameraManager manager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
-        try {
-            for (String cameraId : manager.getCameraIdList()) {
-                CameraCharacteristics characteristics
-                        = manager.getCameraCharacteristics(cameraId);
-
-                if (characteristics.get(CameraCharacteristics.LENS_FACING)
-                        == CameraCharacteristics.LENS_FACING_FRONT) {
-                    continue;
-                }
-
-                StreamConfigurationMap map = characteristics.get(
-                        CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
-
-                mPreviewSize = Collections.max(
-                        Arrays.asList(map.getOutputSizes(ImageFormat.YUV_420_888)),
-                        new CompareSizesByArea());
-                mCameraId = cameraId;
-                return;
-            }
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
     private void createCameraPreviewSession() {
         try {
-            mCameraSurfaceTexture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+            mCameraSurfaceTexture.setDefaultBufferSize(1024, 768);
             Surface surface = new Surface(mCameraSurfaceTexture);
-            mPreviewRequestBuilder
-                    = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            mPreviewRequestBuilder.addTarget(surface);
-
-            mCameraDevice.createCaptureSession(Collections.singletonList(surface),
-                    new CameraCaptureSession.StateCallback() {
-
-                        @Override
-                        public void onConfigured(CameraCaptureSession cameraCaptureSession) {
-                            // The camera is already closed
-                            if (null == mCameraDevice) {
-                                return;
-                            }
-
-                            mCaptureSession = cameraCaptureSession;
-                            try {
-                                mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AF_MODE,
-                                        CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE);
-                                mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_MODE,
-                                        CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH);
-                                mPreviewRequest = mPreviewRequestBuilder.build();
-                                mCaptureSession.setRepeatingRequest(mPreviewRequest,
-                                        null, mBackgroundHandler);
-                            } catch (CameraAccessException e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                        @Override
-                        public void onConfigureFailed(CameraCaptureSession cameraCaptureSession) {
-                            System.out.println("onConfigureFailed FAILED");
-                        }
-                    }, mCaptureHandler
-            );
-        } catch (CameraAccessException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void openCamera() {
-        CameraManager manager = (CameraManager) getContext().getSystemService(Context.CAMERA_SERVICE);
-        try {
-            manager.openCamera(mCameraId, mCameraCallback, mBackgroundHandler);
-        } catch (CameraAccessException e) {
+            Camera c = Camera.open();
+            c.setDisplayOrientation(90);
+            c.setPreviewTexture(mCameraSurfaceTexture);
+            c.startPreview();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
